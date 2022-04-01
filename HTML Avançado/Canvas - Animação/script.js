53f533588d67a2d81f8037a4ccc00f78ca6b6ead
amandakicke <amandakicke@gmail.com>
2022-04-01 10:33:42 -0300

    commit

---
+++ HTML Avançado/Canvas - Animação/script.js
@@ -0,0 +1,34 @@
+let tela = document.getElementById("tela");
+
+let ctx = tela.getContext("2d");
+
+
+
+let circle = {
+    x: 250,
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 0,
+   
+}
+
+function drawCircle(c) {
+
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+    ctx.fill();
+    ctx.stroke();
+}
+
+setInterval(function(){
+    if (circle.fim < 2 * Math.PI) {
+        circle.fim += 0.3;
+        circle.x += 3;
+    }
+
+    drawCircle(circle);
+
+}, 40);
